98774ec42f7ef621eeca54a2ef83fd11e8004092
Christopher Spradlin <cs12230@GeorgiaSouthern.edu>
2019-10-25 23:30:34 -0400

    made static folder visible to node

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ app.use(session({
     }
 }));
 
+app.use('/static', express.static('static'));
 
 // This middleware will check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
 // This usually happens when you stop your express server after login, your cookie still remains saved in the browser.
