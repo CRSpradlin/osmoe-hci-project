1c789eee63676fe752796e6fc25dea1ff8dae3f0
Brian Cox <brian.cox07@gmail.com>
2019-10-25 23:41:11 -0400

    Update home template

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ app.get('/dashboard', (req, res) => {
     if (req.session.user && req.cookies.user_sid) {
         let user = req.session.user;
         console.log(user.email + " has logged in...");
-        res.render('./home.pug', {title:"The Title", message:"Welcome Home " + user.username, happinessBtn:"Increase Happiness", 
+        res.render('./home.pug', {title:"The Title", message:"Osmoe's Daily Inspirational Quote:", happinessBtn:"Increase Happiness", 
         waterBtn:"Give Osmoe Water", foodBtn:"Feed Osmoe", statusLbl:"Osmoe's Status:", happyLbl:"Happiness", waterLbl:"Water", foodLbl:"Food"});
     } else {
         res.redirect('/login');
