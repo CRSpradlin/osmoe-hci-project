9d7816033b433806621be8bd4028d2a244383013
Christopher Spradlin <cs12230@GeorgiaSouthern.edu>
2019-11-24 18:05:40 -0500

    adding osmoeActions.js

---
+++ static/js/views/home/osmoeActions.js
@@ -0,0 +1,10 @@
+let frametime = {
+    'OSIdleX11.gif': 600,
+    'Poke_Idle_OsmoeSam.gif': 900,
+    'Sad_withShadowX10.gif': 1050, 
+}
+let osmoeInterval;
+let idleImage = 'OSIdleX11.gif'
+function setIdle(image){
+
+}
