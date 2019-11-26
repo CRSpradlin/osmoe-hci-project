2a215b18943b2cfb1b918160f119adfc1a5842b0
Christopher Spradlin <cs12230@GeorgiaSouthern.edu>
2019-11-25 19:14:14 -0500

    Added Osmoe Actions logic

--- static/js/views/home/osmoeActions.js
+++ static/js/views/home/osmoeActions.js
@@ -1,10 +1,29 @@
-let frametime = {
+let animationTime = {
     'OSIdleX11.gif': 600,
     'Poke_Idle_OsmoeSam.gif': 900,
     'Sad_withShadowX10.gif': 1050, 
 }
 let osmoeInterval;
-let idleImage = 'OSIdleX11.gif'
-function setIdle(image){
-
+let defaultOsmoe = "OSIdleX11.gif" //change this for crying osmoe
+let currOsmoeImage = "";
+let osmoePending = [];
+let idleImage = 'OSIdleX11.gif';
+function setOsmoe(image){
+    try{
+        clearInterval(osmoeInterval);
+    } catch(e){
+        console.log(e);
+    }
+    currOsmoeImage = image;
+    osmoeInterval = setInterval(()=>{
+        requestAnimationFrame(()=>{document.getElementById("osmoe").src = '/static/penguin/osmoe/animated/'+currOsmoeImage});
+        if((osmoePending.length==0 && currOsmoeImage!=defaultOsmoe) || osmoePending.length>0){
+            if(osmoePending.length>0){
+                setOsmoe(osmoePending.shift());
+            } else {
+                setOsmoe(defaultOsmoe);
+            }
+        }
+    }, animationTime[currOsmoeImage]);
 }
+setOsmoe(defaultOsmoe);
